22244dc4b7a74f6453bd4cddbcbf0a7cd649ce55
104cubes <104cubes@gmail.com>
2022-08-19 11:59:50 +0200

    php errors

--- hola-background/form.php
+++ hola-background/form.php
@@ -1,7 +1,11 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
+        ini_set('display_errors', '1');
+        ini_set('display_startup_errors', '1');
+        error_reporting(E_ALL);
+     header("Access-Control-Allow-Origin: *");
      header("Content-Type: application/json; charset=UTF-8");
      header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, FETCH, OPTIONS");
+     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
      $data = json_decode(file_get_contents('php://input'), true);
      echo json_encode($data);
     exit;
